445fd52c33bdd971b2d6e4fe9c2a482f57373833
Assan <kabayevassan@gmail.com>
2022-05-23 16:49:09 +0600

    Login, Registration, contact

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ContactFormRequest;
+use App\Mail\ContactForm;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class IndexController extends Controller
+{
+    public function index(){
+        $posts = Post::orderBy("created_at", "DESC")->limit(3)->get();
+
+
+        return view('welcome', [
+            "posts" => $posts,
+        ]);
+    }
+
+    public function showContactForm(){
+        return view('contact_form');
+    }
+
+    public function contactFrom(ContactFormRequest $request){
+        Mail::to("kabayevassan@yandex.kz")->send(new ContactForm($request->validated()));
+
+        return redirect(route("contacts"));
+    }
+}
